47020474d32ca6ba4813a94d47c4bad87667a7e4
Capacitor Set <arcadiagiulio@gmail.com>
2014-07-31 21:46:59 +0200

    ...third. Fuck.

--- createInstaller.js
+++ createInstaller.js
@@ -6,8 +6,8 @@ bin_64 = fs.readFileSync('hotspot').toString('base64');
 server_64 = fs.readFileSync('server.js').toString('base64');
 rmtrack_64 = fs.readFileSync('rmtrack').toString('base64');
 
-setup =  '#!/bin/bash';
-setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs';
+setup =  '#!/bin/bash\n';
+setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs\n';
 /* 
 	coreutils: required for base64
 	dnsmasq:   DHCP server
@@ -15,7 +15,8 @@ setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs';
 	conntrack: required by rmtrack
 	nodejs:    required by server.js
 */
-setup += 'echo "' + bin_64     + '" | base64 -d > /usr/bin/hotspot\n';
-setup += 'echo "' + server_64  + '" | base64 -d > /usr/bin/server.js\n'
-setup += 'echo "' + rmtrack_64 + '" | base64 -d > /usr/bin/rmtrack'
-fs.writeFileSync("install.sh", setup);
+setup += 'echo "' + bin_64     + '" | base64 -d | cat > /usr/bin/hotspot\n';
+setup += 'echo "' + server_64  + '" | base64 -d | cat > /usr/bin/server.js\n';
+setup += 'echo "' + rmtrack_64 + '" | base64 -d | cat > /usr/bin/rmtrack\n';
+setup += 'chmod +x /usr/bin/hotspot\n';
+fs.writeFileSync("install.sh", setup);;
