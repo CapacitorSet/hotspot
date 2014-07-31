cc09d4cd0ab43006d6856cc65320c4da4d9001c1
Capacitor Set <arcadiagiulio@gmail.com>
2014-07-31 23:56:19 +0200

    Added profiles

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ function exec(command, callback) {
 	}
 }
 
+profiles = JSON.parse(fs.readFileSync('profiles.json').toString("utf8"));
+
 http.createServer(function(request, response) {
 	var uri = url.parse(request.url).pathname,
 		ip = request.connection.remoteAddress;
@@ -22,28 +24,25 @@ http.createServer(function(request, response) {
 	if (request.headers.host != '192.168.254.1') {
 		response.write('Per navigare devi fare il login!<br><a href="http://192.168.254.1">Vai alla pagina principale</a>');
 	} else if (uri == "/unlock.htm") {
-		exec("sudo arp -an " + ip, function(error, stdout, stderr) {
-			mac = stdout.match(/..:..:..:..:..:../);
-			console.log(stdout, mac);
-			exec('sudo iptables -I internet 1 -t mangle -m mac --mac-source ' + mac + ' -j RETURN', function(error, stdout, stderr) {
-				console.log(stdout);
-			});
-			exec('sudo rmtrack ' + ip, function(error, stdout, stderr) {
-				console.log(stdout);
-			});
-		})
-	} else if (uri == "/unlockcompitigratis.htm") {
-		console.log('a')
-		exec("sudo arp -an " + ip, function(error, stdout, stderr) {
-			mac = stdout.match(/..:..:..:..:..:../);
-			console.log(stdout, mac);
-			exec('iptables -I internet 1 -t mangle -p tcp -d compitigratis.it --dport 80 -m mac --mac-source ' + mac + ' -j RETURN');
-			exec('sudo rmtrack ' + ip, function(error, stdout, stderr) {
-				console.log(stdout);
-			});
+		profile = require('url').parse(request.url, true).query['profile'];
+		profile = profiles[profile]; // deal with it
+		profile.forEach(function (item) {
+			exec("sudo arp -an " + ip, function(error, stdout, stderr) {
+				mac = stdout.match(/..:..:..:..:..:../);
+				console.log(stdout, mac);
+				exec('iptables -I internet 1 -t mangle -p tcp -d ' + item.host + ' --dport ' + item.port + ' -m mac --mac-source ' + mac + ' -j RETURN');
+				exec('sudo rmtrack ' + ip, function(error, stdout, stderr) {
+					console.log(stdout);
+				});
 		});
+
+		})
 	} else {
-		response.write('Benvenuto al captive portal!<br><a href="unlock.htm">Sblocca tutto</a><br><a href="unlockcompitigratis.htm">Sblocca compitigratis</a>');
+		response.write('Benvenuto al captive portal!');
+		for(var profile in profiles) {
+			console.log(profile);
+			response.write('<br><a href="unlock.htm?profile=' + profile + '">Sblocca ' + profile + '</a>');
+		}
 	}
 	response.end();
 	console.log(uri);
