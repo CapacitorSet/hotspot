59119e251e3463625cb692d91526223f9aa05aa2
Capacitor Set <arcadiagiulio@gmail.com>
2014-08-02 00:09:12 +0200

    Simplified createInstaller.js

--- createInstaller.js
+++ createInstaller.js
@@ -4,6 +4,17 @@ var fs = require('fs'),
 	profiles = JSON.parse(fs.readFileSync('profiles.json').toString('utf8')),
 	hotspot = fs.readFileSync('hotspot').toString('utf8');
 
+setup =  '#!/bin/bash\n';
+setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs\n';
+/* 
+	coreutils: 			required for base64
+	dnsmasq: 			DHCP server
+	hostapd:			wireless utility
+	conntrack:			required by rmtrack
+	nodejs:				required by server.js
+*/
+
+// Parses the whitelist and writes it as a set of iptables rules
 whitelist = profiles.whitelist;
 replacement = "";
 whitelist.forEach(function (item) {
@@ -18,6 +29,7 @@ whitelist.forEach(function (item) {
 });
 hotspot = hotspot.replace("# INSERT WHITELIST HERE - DO NOT REPLACE THIS LINE IF YOU DON'T KNOW WHAT YOU'RE DOING", replacement);
 
+// Parses the blacklist and writes it as a set of iptables rules
 blacklist = profiles.blacklist;
 replacement = "";
 blacklist.forEach(function (item) {
@@ -31,28 +43,16 @@ blacklist.forEach(function (item) {
 	replacement += "-j DROP\n"
 });
 hotspot = hotspot.replace("# INSERT BLACKLIST HERE - DO NOT REPLACE THIS LINE IF YOU DON'T KNOW WHAT YOU'RE DOING", replacement);
-
 hotspot_64 = Buffer(hotspot).toString('base64');
+setup += 'echo "' + hotspot_64 + '" | base64 -d | cat > /usr/bin/hotspot\n';
+
+JSON.parse(fs.readFileSync('files.json').toString('utf8')).forEach(function(file) {
+	setup += 'echo "' + fs.readFileSync(file.local).toString('base64') + '" | base64 -d | cat > ' + file.installed + '\n';
+})
 server_64 = fs.readFileSync('server.js').toString('base64');
 rmtrack_64 = fs.readFileSync('rmtrack').toString('base64');
-// dhcpd_64 = fs.readFileSync('dhcpd.conf').toString('base64');
 profiles_64 = fs.readFileSync('profiles.json').toString('base64');
 
-setup =  '#!/bin/bash\n';
-setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs\n';
-/* 
-	coreutils: 			required for base64
-	dnsmasq: 			DHCP server
-	hostapd:			wireless utility
-	conntrack:			required by rmtrack
-	nodejs:				required by server.js
-*/
-// setup += 'echo "' + dhcpd_64   + '" | base64 -d | cat > /etc/dhcp/dhcpd.conf\n';
-setup += 'echo "' + hotspot_64 + '" | base64 -d | cat > /usr/bin/hotspot\n';
-setup += 'echo "' + server_64  + '" | base64 -d | cat > /usr/bin/server.js\n';
-setup += 'echo "' + rmtrack_64 + '" | base64 -d | cat > /usr/bin/rmtrack\n';
-setup += 'echo "' + profiles_64+ '" | base64 -d | cat > /usr/bin/profiles.json\n';
 setup += 'chmod +x /usr/bin/hotspot\n';
-setup += ''
 
 fs.writeFileSync("install.sh", setup);
