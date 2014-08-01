6d368f0ee01ad0943a52f8e002e00734bf194d49
Capacitor Set <arcadiagiulio@gmail.com>
2014-08-01 14:33:52 +0200

    The global whitelist can now be configured more easily with createInstaller.js; bugfix in hotspot

--- createInstaller.js
+++ createInstaller.js
@@ -2,7 +2,14 @@
 
 var fs = require('fs');
 
-bin_64 = fs.readFileSync('hotspot').toString('base64');
+whitelist = JSON.parse(fs.readFileSync('profiles.json').toString('utf8')).whitelist;
+replacement = "";
+whitelist.forEach(function (item) {
+	replacement += "iptables -I internet 1 -t mangle -p tcp -d " + item.host + " --dport " + item.port + " -j RETURN\n"
+});
+hotspot = fs.readFileSync('hotspot').toString('utf8').replace("# INSERT WHITELIST HERE - DO NOT REPLACE THIS LINE IF YOU DON'T KNOW WHAT YOU'RE DOING", replacement);
+
+hotspot_64 = Buffer(hotspot).toString('base64');
 server_64 = fs.readFileSync('server.js').toString('base64');
 rmtrack_64 = fs.readFileSync('rmtrack').toString('base64');
 dhcpd_64 = fs.readFileSync('dhcpd.conf').toString('base64');
@@ -10,15 +17,16 @@ dhcpd_64 = fs.readFileSync('dhcpd.conf').toString('base64');
 setup =  '#!/bin/bash\n';
 setup += 'apt-get install coreutils isc-dhcp-server hostapd conntrack nodejs\n';
 /* 
-	coreutils: required for base64
-	isc-dhcp-server:   DHCP server
-	hostapd:   wireless utility
-	conntrack: required by rmtrack
-	nodejs:    required by server.js
+	coreutils: 			required for base64
+	isc-dhcp-server:	DHCP server
+	hostapd:			wireless utility
+	conntrack:			required by rmtrack
+	nodejs:				required by server.js
 */
-setup += 'echo "' + dhcpd_64     + '" | base64 -d | cat > /etc/dhcp/dhcpd.conf\n';
-setup += 'echo "' + bin_64     + '" | base64 -d | cat > /usr/bin/hotspot\n';
+setup += 'echo "' + dhcpd_64   + '" | base64 -d | cat > /etc/dhcp/dhcpd.conf\n';
+setup += 'echo "' + hotspot_64 + '" | base64 -d | cat > /usr/bin/hotspot\n';
 setup += 'echo "' + server_64  + '" | base64 -d | cat > /usr/bin/server.js\n';
 setup += 'echo "' + rmtrack_64 + '" | base64 -d | cat > /usr/bin/rmtrack\n';
 setup += 'chmod +x /usr/bin/hotspot\n';
+
 fs.writeFileSync("install.sh", setup);
