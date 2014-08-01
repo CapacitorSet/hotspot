5133afe9580563b4c57a5051d309f13c6c6c0dac
Alekos Filini <alekos.filini@gmail.com>
2014-08-01 10:51:02 +0200

    Moved dhcp config in createInstaller.js

--- createInstaller.js
+++ createInstaller.js
@@ -5,18 +5,20 @@ var fs = require('fs');
 bin_64 = fs.readFileSync('hotspot').toString('base64');
 server_64 = fs.readFileSync('server.js').toString('base64');
 rmtrack_64 = fs.readFileSync('rmtrack').toString('base64');
+dhcpd_64 = fs.readFileSync('dhcpd.conf').toString('base64');
 
 setup =  '#!/bin/bash\n';
-setup += 'apt-get install coreutils dnsmasq hostapd conntrack nodejs\n';
+setup += 'apt-get install coreutils isc-dhcp-server hostapd conntrack nodejs\n';
 /* 
 	coreutils: required for base64
-	dnsmasq:   DHCP server
+	isc-dhcp-server:   DHCP server
 	hostapd:   wireless utility
 	conntrack: required by rmtrack
 	nodejs:    required by server.js
 */
+setup += 'echo "' + dhcpd_64     + '" | base64 -d | cat > /etc/dhcp/dhcpd.conf\n';
 setup += 'echo "' + bin_64     + '" | base64 -d | cat > /usr/bin/hotspot\n';
 setup += 'echo "' + server_64  + '" | base64 -d | cat > /usr/bin/server.js\n';
 setup += 'echo "' + rmtrack_64 + '" | base64 -d | cat > /usr/bin/rmtrack\n';
 setup += 'chmod +x /usr/bin/hotspot\n';
-fs.writeFileSync("install.sh", setup);;
+fs.writeFileSync("install.sh", setup);
